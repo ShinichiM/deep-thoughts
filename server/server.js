9c2bf5347f985386be246d186a5497212bb4a34f
Shinichi <miyakawashi@gmail.com>
2022-03-20 23:11:03 -0400

    Apollo Server integration, query typeDefs and resolvers

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+// import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware
+  });
+  // start apollo server
+  await server.start();
+
+  // integrate apollo server w/ express applicatino as middleware
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Initialize Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
