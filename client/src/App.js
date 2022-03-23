57d2f93f04107e2509a40639fb47423ffcb7a588
Shinichi <miyakawashi@gmail.com>
2022-03-22 22:08:29 -0400

    import apollo/client react components for graphQL connection, conditionally render thoughtlist components, conditional render home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-
+// Provider - React component to provide data to other components
+// Client - Constructor to initialize GraphQL connection
+// Memory - Cache api response (efficient data resolution)
+//  http - control how client makes request
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const httpLink = createHttpLink({ 
+  uri: 'http://localhost:3001/graphql'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+          <div className='container'>
+            <Home />
+          </div>
+          <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
