a6981167d9fec7b3037b2d209e5666b0242df24a
Shinichi <miyakawashi@gmail.com>
2022-03-22 22:40:54 -0400

    proxy and relative pathing for client side graphql connection, consolidate server starts on root directory

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+
 // import Apollo Server
 const { ApolloServer, AuthenticationError } = require('apollo-server-express');
 
@@ -37,6 +39,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
