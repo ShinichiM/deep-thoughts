1960f7f9edef07ee416967de662cc2419651183d
Shinichi <miyakawashi@gmail.com>
2022-03-24 00:00:11 -0400

    authentication class construction with localstorage token, signup and login pages implementing token access, App setContext for token headers

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 // Memory - Cache api response (efficient data resolution)
 //  http - control how client makes request
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -19,10 +20,22 @@ const httpLink = createHttpLink({
   uri: '/graphql'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
+
+
 
 function App() {
   return (
---
+++ client/src/utils/auth.js
@@ -0,0 +1,51 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+    //  retrieve data saved in token
+    getProfile() {
+        return decode(this.getToken());
+    }
+
+    // check if the user is stil logged in
+    loggedIn() {
+        // checks if there is a saved token and still valid
+        const token = this.getToken();
+        // use type coersion to check if token is Not undefined and token is not expired
+        return !!token && !this.isTokenExpired(token);
+    } 
+
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch(err) {
+            return false;
+        }
+    }
+    // retrieve token from localStorage
+    getToken() {
+        // retrieves user token from localstorage
+        return localStorage.getItem('id_token')
+    }
+    // set token to localStorage and reload page to homepage
+    login(idToken) {
+        // save user token to localStorage
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/')
+    }
+
+    // clear token from localstorage and force logout with reload
+    logout() {
+        // clear token and profile data from localstorage
+        localStorage.removeItem('id_token');
+        // reload page and reset the state of application
+        window.location.assign('/')
+    }
+}
+
+export default new AuthService();
